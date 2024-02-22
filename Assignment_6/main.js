0e94acee5887d4d0e6f15fcc86a4b4d92ab4651a
Elijah Dromarsky <157527162+CanadianBias@users.noreply.github.com>
2024-02-22 14:06:50 -0600

    Using bibleBooks.json from Assignment 5 to populate HTML elements

--- Assignment_6/main.js
+++ Assignment_6/main.js
@@ -11,10 +11,41 @@ async function printText() {
     //console.log(promise);
     if (response.ok) {
         let json = await response.json();
-        // console.log(json);
-        document.getElementById("testElement").innerHTML = json;
+        console.log(json);
+        //document.getElementById("testElement").innerHTML = json.text;
     } else {
         alert("HTTPS-Error: " + response.status);
     }
+}
+
+// function that grabs the JSON file data and populates the dropdown with it
+async function populateDropdown() {
+    let response = await fetch("bibleBooks.json");
+    //console.log(promise);
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+        // for (i=0;i<=66;i++) {
+        //     document.getElementById("bibleBookDropdown").innerHTML += "<option value='" + json.books[i].book + "'>" + json.books[i].book + "</option>";
+        // }
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+populateDropdown();
+
+// function to create objects when data is submitted
+// sourced from https://www.learnwithjason.dev/blog/get-form-values-as-json/
+function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    // Do a bit of work to convert the entries to a plain JS object
+    const value = Object.fromEntries(data.entries());
+
+    //console.log({ value });
+    //console.log(value)
+
 }
 printText();
