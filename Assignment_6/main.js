970ba4d99163af732514393b9779f0ef085c69ae
Elijah Dromarsky <elidenviro@outlook.com>
2024-02-27 16:32:13 -0600

    testing printText function

--- Assignment_6/main.js
+++ Assignment_6/main.js
@@ -18,49 +18,54 @@ async function printText(strURL) {
         alert("HTTPS-Error: " + response.status);
     }
 }
+printText("https://bible-api.com/jn%203:16");
 
-// This database works to populate the bible books and chapters, but it doesn't tell you how many verses are per chapter
-// function that grabs the JSON file data and populates the dropdown with it
-// Might not be very useful if using text inputs, unless there's some way to check against it
-async function populateBookDropdown() {
-    let response = await fetch("bibleBooks.json");
-    //console.log(promise);
-    if (response.ok) {
-        let json = await response.json();
-        console.log(json);
-        for (i=0;i<=66;i++) {
-            // Format for adding elements to select dropdown sourced from https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/add
-            let sel = document.getElementById("bibleBookDropdown"); // Creates a reference to HTML object
-            
-            let opt = document.createElement("option");
-            opt.value = json.books[i].book;
-            opt.text = json.books[i].book;
-            
-            sel.add(opt, null);
-        }
-    } else {
-        alert("HTTP-Error: " + response.status);
-    }
-}
+// function buttonClicked() {
 
-// function to create objects when data is submitted
-// sourced from https://www.learnwithjason.dev/blog/get-form-values-as-json/
-function handleSubmit(event) {
-    event.preventDefault();
+// }
+
+
+// // This database works to populate the bible books and chapters, but it doesn't tell you how many verses are per chapter
+// // function that grabs the JSON file data and populates the dropdown with it
+// // Might not be very useful if using text inputs, unless there's some way to check against it
+// async function populateBookDropdown() {
+//     let response = await fetch("bibleBooks.json");
+//     //console.log(promise);
+//     if (response.ok) {
+//         let json = await response.json();
+//         console.log(json);
+//         for (i=0;i<=66;i++) {
+//             // Format for adding elements to select dropdown sourced from https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/add
+//             let sel = document.getElementById("bibleBookDropdown"); // Creates a reference to HTML object
+            
+//             let opt = document.createElement("option");
+//             opt.value = json.books[i].book;
+//             opt.text = json.books[i].book;
+            
+//             sel.add(opt, null);
+//         }
+//     } else {
+//         alert("HTTP-Error: " + response.status);
+//     }
+// }
+
+// // function to create objects when data is submitted
+// // sourced from https://www.learnwithjason.dev/blog/get-form-values-as-json/
+// function handleSubmit(event) {
+//     event.preventDefault();
     
-    const data = new FormData(event.target);
+//     const data = new FormData(event.target);
     
-    // Do a bit of work to convert the entries to a plain JS object
-    const value = Object.fromEntries(data.entries());
+//     // Do a bit of work to convert the entries to a plain JS object
+//     const value = Object.fromEntries(data.entries());
     
-    //console.log({ value });
-    //console.log(value)
+//     //console.log({ value });
+//     //console.log(value)
     
-}
+// }
 
 // async function calls
-populateBookDropdown();
-printText("https://bible-api.com/jn%203:16");
+// populateBookDropdown();
 
 // let selectBook = document.getElementById("bibleBookDropdown");
 
