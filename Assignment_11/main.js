bf4405064c6ce15615d937f92f45a813fc92bd02
Elijah Dromarsky <157527162+CanadianBias@users.noreply.github.com>
2024-04-23 13:54:37 -0500

    Added comments and assingment requirements

--- Assignment_11/main.js
+++ Assignment_11/main.js
@@ -1,15 +1,30 @@
+/* 
+≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+≡≡≡≡≡ Elijah Dromarsky ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+≡≡≡≡≡ edromarsky@emmaus.edu ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+≡≡≡≡≡ Creating JS webpages for educational purposes ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+≡≡≡≡≡ Intellectual Property of Elijah Dromarsky, All Rights Reserved ≡≡≡≡≡≡≡≡≡≡≡≡≡
+≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+*/
+
+// Code needed for Google to use charts
+google.charts.load('current',{packages:['corechart']});
+google.charts.setOnLoadCallback(drawGooglePieChart);
+
+// This function pulls from the Canvas Table, and draws points on a canvas based on the input in the table.
 function drawScatterCanvas() {
     const myTable = document.getElementById("canvasTable"); // grabs reference to table
     let xArray = [];
     let yArray = [];
     for (let i = 1; i < 9; i++) { // iterates through table rows and grabs value in text input, stores to array
-        xArray.push(parseFloat(myTable.children[0].children[i].children[0].children[0].value));
+        xArray.push(parseFloat(myTable.children[0].children[i].children[0].children[0].value)); // grabs value from table
         yArray.push(parseFloat(myTable.children[0].children[i].children[1].children[0].value));
     }
     
     const myCanvas = document.getElementById("thisCanvas"); // grabs reference to HTML canvas element
     const ctx = myCanvas.getContext("2d"); // sets 2D context for graphing
 
+    // clears screen to avoid just writing over the old canvas
     ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
 
     // Adjusts orgin to be in the bottom right corner
@@ -36,6 +51,7 @@ function drawScatterCanvas() {
 
 }
 
+// This function is similar to the above function, but draws a (very) rough linear line along with the data as well
 function drawLineCanvas() {
     const myTable = document.getElementById("canvasTable"); // grabs reference to table
     let xArray = [];
@@ -84,6 +100,7 @@ function drawLineCanvas() {
 
 }
 
+// Draws Bar Graph from Bar Graph table using the Plotly.js library, with associated script tag in the HTML document
 function drawBarPlotly() {
     const myTable = document.getElementById("barTable"); // grabs reference to table
     let xArray = [];
@@ -95,20 +112,22 @@ function drawBarPlotly() {
         }
     }
 
+    // Writes the data from the arrays in a way that Plotly can understand it
     const data = [{
         x: xArray,
         y: yArray,
-        type: "bar",
-        orientation: "v",
+        type: "bar", // tells plotly to make it a bar graph
+        orientation: "v", // vertical orientation for the bar graph
         marker: {color:"rgba(0,0,255)"}
     }];
 
-    const layout = {title:"Graph Information"};
+    const layout = {title:"Graph Information"}; // sets graph title
 
-    Plotly.newPlot("barPlot", data, layout);
+    Plotly.newPlot("barPlot", data, layout); // creates graph
 
 }
 
+// Draws a Pie Graph from respective table using Plotly.js library
 function drawPiePlotly() {
     const myTable = document.getElementById("pieTable"); // grabs reference to table
     let xArray = [];
@@ -120,18 +139,20 @@ function drawPiePlotly() {
         }
     }
 
+    // Writes the data from the arrays in a way that Plotly can understand it
     const data = [{
         labels: xArray,
         values: yArray,
-        type: "pie"
+        type: "pie" // tells plotly.js to make it a pie graph
     }];
 
-    const layout = {title: "Pie Graph with Data"};
+    const layout = {title: "Pie Graph with Data"}; // sets graph title
 
-    Plotly.newPlot("piePlot", data, layout);
+    Plotly.newPlot("piePlot", data, layout); // creates graph
 
 }
 
+// Same  pie graph as above, just with a hole in the middle
 function drawDonutPlotly() {
     const myTable = document.getElementById("pieTable"); // grabs reference to table
     let xArray = [];
@@ -146,7 +167,7 @@ function drawDonutPlotly() {
     const data = [{
         labels: xArray,
         values: yArray,
-        hole: .4,
+        hole: .4, // adds the hole to make it a donut graph
         type: "pie"
     }];
 
@@ -156,13 +177,14 @@ function drawDonutPlotly() {
 
 }
 
+// draws two sine functions with different frequencies using Plotly, not interactive with user but for demonstration
 function drawSineGraphs() {
  
     const xArray1 = [];
     const yArray1 = [];
     for (let x = 0; x < 10; x += 0.1) {
         xArray1.push(x);
-        yArray1.push(Math.sin(x));
+        yArray1.push(Math.sin(x)); // creates yArray as function of the xArray
     }
     const xArray2 = [];
     const yArray2 = [];
@@ -170,11 +192,197 @@ function drawSineGraphs() {
         xArray2.push(x);
         yArray2.push(Math.sin(2*x));
     }
-    const data1 = [{x:xArray1, y:yArray1, mode:"lines"}];
-    const data2 = [{x:xArray2, y:yArray2, mode:"lines"}];
-    const layout = {title: "Two Sine Functions With Different Frequencies"};
-    // const data = [data1, data2];
-    Plotly.newPlot("sinePlot", data2, layout);
+    const data1 = {x:xArray1, y:yArray1, mode:"lines"};
+    const data2 = {x:xArray2, y:yArray2, mode:"lines"};
+    const data = [ data1, data2 ]; // adds both functions to be able to be drawn
+    const layout = {title: "Two Sine Functions With Different Frequencies"}; // title information
+    Plotly.newPlot("sinePlot", data, layout);
+}
+
+function drawBarChart() {
+    const myTable = document.getElementById("barTable"); // grabs reference to table
+    let xArray = [];
+    let yArray = [];
+    for (let i = 1; i < 9; i++) { // iterates through table rows and grabs value in text input, stores to array
+        if (myTable.children[0].children[i].children[0].children[0].value != "" || myTable.children[0].children[i].children[1].children[0].value != "") {
+            xArray.push(myTable.children[0].children[i].children[0].children[0].value);
+            yArray.push(parseFloat(myTable.children[0].children[i].children[1].children[0].value));
+        }
+    }
+
+    const myCanvas = document.getElementById("barCanvas"); // grabs reference to HTML canvas element
+    const ctx = myCanvas.getContext("2d"); // sets 2D context for graphing
+
+    const barColors = "blue";
+
+    // initializes new chart
+    new Chart("barCanvas", {
+        type: "bar",
+        data: {
+            labels: xArray,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yArray
+            }]
+          },
+        options: {
+            title: {
+                display: true,
+                text: "Chart.js Bar Graph"
+            }
+        }
+      });
+}
+
+// Draws a Pie Chart with Chart.js
+function drawPieChart() {
+    const myTable = document.getElementById("pieTable"); // grabs reference to table
+    let xArray = [];
+    let yArray = [];
+    for (let i = 1; i < 9; i++) { // iterates through table rows and grabs value in text input, stores to array
+        if (myTable.children[0].children[i].children[0].children[0].value != "" || myTable.children[0].children[i].children[1].children[0].value != "") {
+            xArray.push(myTable.children[0].children[i].children[0].children[0].value);
+            yArray.push(parseFloat(myTable.children[0].children[i].children[1].children[0].value));
+        }
+    }
+
+    const myCanvas = document.getElementById("pieCanvas"); // grabs reference to HTML canvas element
+    const ctx = myCanvas.getContext("2d"); // sets 2D context for graphing
+    const barColors = ["blue", "red", "green", "yellow", "brown", "orange", "purple", "grey"];
+
+    // initializes new chart
+    new Chart("pieCanvas", {
+        type: "pie",
+        data: {
+          labels: xArray, // sets categories
+          datasets: [{
+            backgroundColor: barColors,
+            data: yArray // assigns categories data
+          }]
+        },
+        options: {
+          title: { // title information
+            display: true,
+            text: "Chart.js Pie Graph"
+          }
+        }
+      });
+
+}
+
+// this one below isn't working currently
+function drawMultipleLinesChart() {
+    const xValues = [100,200,300,400,500,600,700,800,900,1000];
+
+    const myCanvas = document.getElementById("lineCanvas"); // grabs reference to HTML canvas element
+    const ctx = myCanvas.getContext("2d"); // sets 2D context for graphing
+
+    new Chart("lineCanvas", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+                borderColor: "red",
+                fill: false
+            },{
+                data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+                borderColor: "green",
+                fill: false
+            },{
+                data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+                borderColor: "blue",
+                fill: false
+            }]
+        },
+        options: {
+            legend: {display: false}
+        }
+    });
+}
+
+// Draws 3D pie chart
+function drawGooglePieChart() {
+
+    const myTable = document.getElementById("pieTable"); // grabs reference to table
+    let xArray = [];
+    let yArray = [];
+    for (let i = 1; i < 9; i++) { // iterates through table rows and grabs value in text input, stores to array
+        if (myTable.children[0].children[i].children[0].children[0].value != "" || myTable.children[0].children[i].children[1].children[0].value != "") {
+            xArray.push(myTable.children[0].children[i].children[0].children[0].value);
+            yArray.push(parseFloat(myTable.children[0].children[i].children[1].children[0].value));
+        }
+    }
+
+    // Set Data
+    let googleData = [];
+    googleData.push(['Header', "Units"]);
+    for (let i = 0; i < xArray.length; i++) {
+        googleData.push([xArray[i], yArray[i]]);
+    }
+
+    const data = google.visualization.arrayToDataTable(googleData);
+
+    // Set Options
+    const options = {
+        title: 'Google Chart Pie Graph',
+        is3D: true
+    };
+
+    // Draw
+    const chart = new google.visualization.PieChart(document.getElementById('googlePieChart'));
+    chart.draw(data, options);
+}
+
+
+// This function mostly ripped from W3 Schools example
+function drawD3Scatter() {
+    // Set Dimensions
+    const xSize = 500;
+    const ySize = 500;
+    const margin = 40;
+    const xMax = xSize - margin*2;
+    const yMax = ySize - margin*2;
+
+    // Create Random Points
+    const numPoints = 100;
+    const data = [];
+    for (let i = 0; i < numPoints; i++) {
+        data.push([Math.random() * xMax, Math.random() * yMax]);
+    }
+
+    // Append SVG Object to the Page
+    const svg = d3.select("#myPlot")
+        .append("svg")
+        .append("g")
+        .attr("transform","translate(" + margin + "," + margin + ")");
+
+    // X Axis
+    const x = d3.scaleLinear()
+        .domain([0, 500])
+        .range([0, xMax]);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + yMax + ")")
+        .call(d3.axisBottom(x));
+
+    // Y Axis
+    const y = d3.scaleLinear()
+        .domain([0, 500])
+        .range([ yMax, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Dots
+    svg.append('g')
+        .selectAll("dot")
+        .data(data).enter()
+        .append("circle")
+        .attr("cx", function (d) { return d[0] } )
+        .attr("cy", function (d) { return d[1] } )
+        .attr("r", 3)
+        .style("fill", "Red");
 }
 
 // initializing function calls
@@ -183,7 +391,11 @@ drawLineCanvas();
 drawBarPlotly();
 drawPiePlotly();
 drawDonutPlotly();
-drawSineGraphs();
+drawSineGraphs(); // this is not called when inputs are changed
+drawBarChart();
+drawPieChart();
+drawLineCanvas(); // this is not called when inputs are changed
+drawD3Scatter(); // called once
 
 let inputList = document.querySelectorAll(".inputBox"); // when new input is recieved to tables, redraw graphs
 for (let i = 0; i < inputList.length; i++) {
@@ -192,8 +404,12 @@ for (let i = 0; i < inputList.length; i++) {
     inputList[i].addEventListener("change", drawBarPlotly);
     inputList[i].addEventListener("change", drawPiePlotly);
     inputList[i].addEventListener("change", drawDonutPlotly);
+    inputList[i].addEventListener("change", drawBarChart);
+    inputList[i].addEventListener("change", drawPieChart);
+    inputList[i].addEventListener("change", function() {google.charts.setOnLoadCallback(drawGooglePieChart);}); // short built-in function to redraw google chart
 }
-window.addEventListener("resize", drawBarPlotly); // needed as Plotly doesn't autoresize with window
+// needed as Plotly doesn't autoresize with window
+window.addEventListener("resize", drawBarPlotly);
 window.addEventListener("resize", drawPiePlotly);
 window.addEventListener("resize", drawDonutPlotly);
 window.addEventListener("resize", drawSineGraphs);
